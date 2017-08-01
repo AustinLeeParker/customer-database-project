5158f217ba1254756cf7706ccb41636b0848dc7b
AustinLeeParker <tehdemopan@gmail.com>
2017-08-01 15:37:10 -0500

    Added HTML and CSS to enhance working functions in page

---
+++ main.js
@@ -0,0 +1,143 @@
+for (i = 0; i < 3; i++) {
+    for (x = 0; i < 4; i++) {
+    }
+    for (x = 0; i < 4; i++) {
+    }
+}
+
+const = 
+
+<div id="img">
+    <img class="img" src="https://randomuser.me/api/portraits/med/men/0.jpg">
+        <img class="img" src="https://randomuser.me/api/portraits/med/men/0.jpg">
+            <img class="img" src="https://randomuser.me/api/portraits/med/men/0.jpg">
+                <img class="img" src="https://randomuser.me/api/portraits/med/men/0.jpg">
+  </div>
+                <div id="top_row">
+                    <div class="text">
+                        <h2> John Doe</h2>
+                        <hr>
+                            <p>Johndoe@mail.com</p>
+                            <p>12345 Street Address</p>
+                            <p> Cool City,GA 303030</p>
+                            <p>555-555-5555</p>
+                            <p class="social">123-45-6789</p>
+    </div>
+                        <div class="text">
+                            <h2> John Doe</h2>
+                            <hr>
+                                <p>Johndoe@mail.com</p>
+                                <p>12345 Street Address</p>
+                                <p> Cool City,GA 303030</p>
+                                <p>555-555-5555</p>
+                                <p class="social">123-45-6789</p>
+    </div>
+                            <div class="text">
+                                <h2> John Doe</h2>
+                                <hr>
+                                    <p>Johndoe@mail.com</p>
+                                    <p>12345 Street Address</p>
+                                    <p> Cool City,GA 303030</p>
+                                    <p>555-555-5555</p>
+                                    <p class="social">123-45-6789</p>
+    </div>
+                                <div class="text">
+                                    <h2> John Doe</h2>
+                                    <hr>
+                                        <p>Johndoe@mail.com</p>
+                                        <p>12345 Street Address</p>
+                                        <p> Cool City,GA 303030</p>
+                                        <p>555-555-5555</p>
+                                        <p class="social">123-45-6789</p>
+    </div>
+                                </div>
+                            </div>
+                            <div id="img">
+                                <img class="img" src="https://randomuser.me/api/portraits/med/men/0.jpg">
+                                    <img class="img" src="https://randomuser.me/api/portraits/med/men/0.jpg">
+                                        <img class="img" src="https://randomuser.me/api/portraits/med/men/0.jpg">
+                                            <img class="img" src="https://randomuser.me/api/portraits/med/men/0.jpg">
+  </div>
+                                            <div id="top_row">
+                                                <div class="text">
+                                                    <h2> John Doe</h2>
+                                                    <hr>
+                                                        <p>Johndoe@mail.com</p>
+                                                        <p>12345 Street Address</p>
+                                                        <p> Cool City,GA 303030</p>
+                                                        <p>555-555-5555</p>
+                                                        <p class="social">123-45-6789</p>
+    </div>
+                                                    <div class="text">
+                                                        <h2> John Doe</h2>
+                                                        <hr>
+                                                            <p>Johndoe@mail.com</p>
+                                                            <p>12345 Street Address</p>
+                                                            <p> Cool City,GA 303030</p>
+                                                            <p>555-555-5555</p>
+                                                            <p class="social">123-45-6789</p>
+    </div>
+                                                        <div class="text">
+                                                            <h2> John Doe</h2>
+                                                            <hr>
+                                                                <p>Johndoe@mail.com</p>
+                                                                <p>12345 Street Address</p>
+                                                                <p> Cool City,GA 303030</p>
+                                                                <p>555-555-5555</p>
+                                                                <p class="social">123-45-6789</p>
+    </div>
+                                                            <div class="text">
+                                                                <h2> John Doe</h2>
+                                                                <hr>
+                                                                    <p>Johndoe@mail.com</p>
+                                                                    <p>12345 Street Address</p>
+                                                                    <p> Cool City,GA 303030</p>
+                                                                    <p>555-555-5555</p>
+                                                                    <p class="social">123-45-6789</p>
+    </div>
+                                                            </div>
+
+                                                            <div id="img">
+                                                                <img class="img" src="https://randomuser.me/api/portraits/med/men/0.jpg">
+                                                                    <img class="img" src="https://randomuser.me/api/portraits/med/men/0.jpg">
+                                                                        <img class="img" src="https://randomuser.me/api/portraits/med/men/0.jpg">
+                                                                            <img class="img" src="https://randomuser.me/api/portraits/med/men/0.jpg">
+  </div>
+                                                                            <div id="top_row">
+                                                                                <div class="text">
+                                                                                    <h2> John Doe</h2>
+                                                                                    <hr>
+                                                                                        <p>Johndoe@mail.com</p>
+                                                                                        <p>12345 Street Address</p>
+                                                                                        <p> Cool City,GA 303030</p>
+                                                                                        <p>555-555-5555</p>
+                                                                                        <p class="social">123-45-6789</p>
+    </div>
+                                                                                    <div class="text">
+                                                                                        <h2> John Doe</h2>
+                                                                                        <hr>
+                                                                                            <p>Johndoe@mail.com</p>
+                                                                                            <p>12345 Street Address</p>
+                                                                                            <p> Cool City,GA 303030</p>
+                                                                                            <p>555-555-5555</p>
+                                                                                            <p class="social">123-45-6789</p>
+    </div>
+                                                                                        <div class="text">
+                                                                                            <h2> John Doe</h2>
+                                                                                            <hr>
+                                                                                                <p>Johndoe@mail.com</p>
+                                                                                                <p>12345 Street Address</p>
+                                                                                                <p> Cool City,GA 303030</p>
+                                                                                                <p>555-555-5555</p>
+                                                                                                <p class="social">123-45-6789</p>
+    </div>
+                                                                                            <div class="text">
+                                                                                                <h2> John Doe</h2>
+                                                                                                <hr>
+                                                                                                    <p>Johndoe@mail.com</p>
+                                                                                                    <p>12345 Street Address</p>
+                                                                                                    <p> Cool City,GA 303030</p>
+                                                                                                    <p>555-555-5555</p>
+                                                                                                    <p class="social">123-45-6789</p>
+    </div>
+                                                                                            </div>
