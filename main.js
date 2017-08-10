d09267b780a6cc8794227f03ef5a7aaf6ae31dc6
AustinLeeParker <tehdemopan@gmail.com>
2017-08-10 13:05:03 -0500

    Edited javascript

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 <script src "customers.js"></script >
-    for (i = 0; i < 3; i++) {
+    let directory;
+directory = ["name", "email", "address", "city", "phone", "ss"]
+
+
+for (i = 0; i < 3; i++) {
+
     for (x = 0; i < 4; i++) {
+
     }
     for (x = 0; i < 4; i++) {
+
     }
 }
