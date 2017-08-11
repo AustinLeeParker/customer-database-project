b052e9cb1a855c7d60ff10f79952f55b640c2fe9
AustinLeeParker <tehdemopan@gmail.com>
2017-08-11 14:28:24 -0500

    Edited js to make directory more effective at displaying data

--- main.js
+++ main.js
@@ -11,14 +11,16 @@
 let container = document.querySelector(".container");
 for (let i = 0; i < customers.results.length; i++) {
     let content =
-        `<div class="info">
-        <h2> John Doe</h2>
-        <hr>
+        `<div class="content">
             <img class="img" src=${customers.results[i].picture.large}>
-                <p>${customers.results[i].name.first} ${customers.results[i].name.last}</p>
+                <h2>${customers.results[i].name.first} ${customers.results[i].name.last}</h2>
+                <hr>
                 <p>${customers.results[i].email}</p>
+                <br>
                 <p>${customers.results[i].location.street}</p>
+                <p>${customers.results[i].location.city} ${customers.results[i].location.state} ${customers.results[i].location.postcode}</p>
                 <p>${customers.results[i].cell}</p>
+                <br>
                 <p class ="social">${customers.results[i].id.value}</p>
         </div>`;
     container.innerHTML += content;
