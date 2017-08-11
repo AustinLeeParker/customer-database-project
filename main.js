dab1e807347ac6c8f52ebbccbda0b1d4cbbb6ef6
AustinLeeParker <tehdemopan@gmail.com>
2017-08-11 14:13:35 -0500

    Made the java script work by using a for loop to produce results

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
-<script src "customers.js"></script >
-    let directory;
-directory = ["name", "email", "address", "city", "phone", "ss"]
 
+// for (i = 0; i < 3; i++) {
 
-for (i = 0; i < 3; i++) {
+//     for (x = 0; i < 4; i++) {
 
-    for (x = 0; i < 4; i++) {
+//     }
+//     for (x = 0; i < 4; i++) {
 
-    }
-    for (x = 0; i < 4; i++) {
-
-    }
+//     }
+// }
+let container = document.querySelector(".container");
+for (let i = 0; i < customers.results.length; i++) {
+    let content =
+        `<div class="info">
+        <h2> John Doe</h2>
+        <hr>
+            <img class="img" src=${customers.results[i].picture.large}>
+                <p>${customers.results[i].name.first} ${customers.results[i].name.last}</p>
+                <p>${customers.results[i].email}</p>
+                <p>${customers.results[i].location.street}</p>
+                <p>${customers.results[i].cell}</p>
+                <p class ="social">${customers.results[i].id.value}</p>
+        </div>`;
+    container.innerHTML += content;
 }
+
+
+
+//   <img class="img" src="https://randomuser.me/api/portraits/med/men/0.jpg">
+//     <h2> John Doe</h2>
+//     <hr>
+//     <p>Johndoe@mail.com</p>
+//     <p>12345 Street Address</p>
+//     <p> Cool City,GA 303030</p>
+//     <p>555-555-5555</p>
+//     <p class="social">123-45-6789</p>
